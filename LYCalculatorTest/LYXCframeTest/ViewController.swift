3922147729416651fcb18ab2d8815dc590d97e5c
bel <bel@beldeMacBook-Pro.local>
2021-03-09 23:06:20 +0800

    xcframework

---
+++ LYCalculatorTest/LYXCframeTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LYXCframeTest
+//
+//  Created by bel on 2021/3/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LYCalculatorTest/LYXCframeTestTests/LYXCframeTestTests.swift
@@ -0,0 +1,33 @@
+//
+//  LYXCframeTestTests.swift
+//  LYXCframeTestTests
+//
+//  Created by bel on 2021/3/9.
+//
+
+import XCTest
+@testable import LYXCframeTest
+
+class LYXCframeTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
